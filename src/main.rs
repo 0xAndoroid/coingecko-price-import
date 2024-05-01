7e06b71bcc542ac8efceb6f00717028802e78272
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2024-05-01 15:23:46 -0600

    optional 24hr change

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use rocket::get;
 use rocket::launch;
 use rocket::routes;
 
-#[get("/price?<ids>&<vs>")]
-async fn price(ids: Option<&str>, vs: Option<&str>) -> String {
+#[get("/price?<ids>&<vs>&<include_24hr_change>&<include_market_cap>")]
+async fn price(
+    ids: Option<&str>,
+    vs: Option<&str>,
+    include_24hr_change: Option<bool>,
+    include_market_cap: Option<bool>,
+) -> String {
     let ids = ids.unwrap();
-    let vs = vs.unwrap();
+    let vs = vs.unwrap_or("usd");
+    let include_24hr_change = include_24hr_change.unwrap_or(false);
+    let include_market_cap = include_market_cap.unwrap_or(false);
     let url =
-        format!("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies={}&include_24hr_change=true&include_market_cap=true&include_24hr_vol=true", ids, vs);
+        format!("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies={}&include_24hr_change={}&include_market_cap={}", ids, vs, include_24hr_change, include_market_cap);
     reqwest::get(url).await.unwrap().text().await.unwrap()
 }
 
