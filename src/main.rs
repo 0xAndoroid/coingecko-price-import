289968de935a8ad7ec6489c14ee659fa536759ab
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2026-04-27 11:59:52 -0400

    chore: update user agent
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use rocket::get;
 use rocket::launch;
 use rocket::routes;
 
-const USER_AGENT: &str = "coingecko-price-import/0.1.1";
+const USER_AGENT: &str = "coingecko-price-import-proxy/0.1.1";
 
 fn client() -> Client {
-    Client::builder().user_agent(USER_AGENT).build().expect("failed to build reqwest client")
+    Client::builder()
+        .user_agent(USER_AGENT)
+        .build()
+        .expect("failed to build reqwest client")
 }
 
 #[get("/price?<ids>&<vs>&<include_24hr_change>&<include_market_cap>")]
@@ -18,17 +21,40 @@ async fn price(
 ) -> String {
     let ids = ids.unwrap();
     let vs = vs.unwrap_or("usd");
-    let include_24hr_change = include_24hr_change.unwrap_or("false").parse::<bool>().unwrap();
-    let include_market_cap = include_market_cap.unwrap_or("false").parse::<bool>().unwrap();
+    let include_24hr_change = include_24hr_change
+        .unwrap_or("false")
+        .parse::<bool>()
+        .unwrap();
+    let include_market_cap = include_market_cap
+        .unwrap_or("false")
+        .parse::<bool>()
+        .unwrap();
     let url =
         format!("https://api.coingecko.com/api/v3/simple/price?ids={ids}&vs_currencies={vs}&include_24hr_change={include_24hr_change}&include_market_cap={include_market_cap}");
-    client().get(url).send().await.unwrap().text().await.unwrap()
+    client()
+        .get(url)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
 }
 
 #[get("/binance?<symbol>")]
 async fn binance(symbol: Option<&str>) -> String {
-    let url = format!("https://api.binance.com/api/v3/avgPrice?symbol={}", symbol.unwrap());
-    client().get(url).send().await.unwrap().text().await.unwrap()
+    let url = format!(
+        "https://api.binance.com/api/v3/avgPrice?symbol={}",
+        symbol.unwrap()
+    );
+    client()
+        .get(url)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
 }
 
 #[launch]
